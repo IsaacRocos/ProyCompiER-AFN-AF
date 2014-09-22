834d77e55b678810854ff7d044459ed4e811e400
Isaac <Isaac@Kronos>
2014-09-22 02:51:49 -0500

    Agregada programación para cerraduras

--- Proyecto1Compiladores/src/afn/AFN.java
+++ Proyecto1Compiladores/src/afn/AFN.java
@@ -1,6 +1,7 @@
 package afn;
 
 import java.util.ArrayList;
+import java.util.EmptyStackException;
 import java.util.Stack;
 
 /**
@@ -10,8 +11,8 @@ import java.util.Stack;
 public class AFN {
 
     private char[] regExp; // Guarda la expresión en posfijo, en un arreglo de caracteres.
-    private Stack<char[]> automata;  //Almacena la información del automata.
-    private Stack<int[]> inicio_fin;
+    private Stack<String[]> automata;  //Almacena la información del automata.
+    private Stack<int[]> inicio_fin; // Pila que almacena los pares de estados [Inicio-Fin] que se generan.
     private int ultimoEstadoInicial = 0;
     private int ultimoEstadoFinal = 0;
     private ArrayList<String> alfabeto;
@@ -36,36 +37,37 @@ public class AFN {
 
     public void iniciarCreaAFN() {
         char elementoExpAnalizar; // alamacena el caracter que se analiza en la expresión regular (sólo un caracter)
-        String elementoAlfAnalizar = "";  // Se usa para poder identificar si el elemento del alfabeto está compuesto por más de un caracter como  "a1" o "bn" , etc.
+        String elementoTansicion = "";  // Se usa para poder identificar si el elemento del alfabeto está compuesto por más de un caracter como  "a1" o "bn" , etc.
         for (int i = 0; i < regExp.length; i++) {
-            System.out.println("Valor de indice: " + i);
+            System.out.println("Analizando elemento " + i + " de la expresion regular...");
             elementoExpAnalizar = regExp[i];
             switch (elementoExpAnalizar) {
                 case '|':
-                    System.out.println("Generando automata para union");
-                    // creaAutomataUnion();
+                    creaAutomataUnion();
                     break;
                 case '-':
                     creaAutomataConcatenacion();
                     break;
                 case '*':
-                    creaAutomataCerraduraEp();
+                    creaAutomataCerradura(0);
                     break;
                 case '+':
-                    creaAutomataCerraduraP();
+                    creaAutomataCerradura(1);
                     break;
-                default://cualquier caracter
+                default://CARACTERES DE TRANSICION
                     try {
-                        elementoAlfAnalizar = "" + elementoExpAnalizar;
-                        while (!alfabeto.contains(elementoAlfAnalizar) || !alfabeto.contains(regExp[i + 1] + "")) {
+                        elementoTansicion = "" + elementoExpAnalizar;
+                        while (!alfabeto.contains(elementoTansicion) || !alfabeto.contains(regExp[i + 1] + "")) {
                             if (regExp[i + 1] != '|' && regExp[i + 1] != '*' && regExp[i + 1] != '-' && regExp[i + 1] != '+') {
-                                elementoAlfAnalizar += regExp[i + 1];
+                                elementoTansicion += regExp[i + 1];
+                            } else {
+                                break;
                             }
-                            System.out.println("Buscando en alfabeto: " + elementoAlfAnalizar);
+                            //System.out.println("Buscando en alfabeto: " + elementoAlfAnalizar);
                             i++;
                         }
-                        System.out.println("Encontrado: " + elementoAlfAnalizar);
-                        //creaAutomataSimple(elementoExpAnalizar);
+                        System.out.println("Encontrado: " + elementoTansicion);
+                        creaAutomataSimple(elementoTansicion);
                     } catch (ArrayIndexOutOfBoundsException aiobe) {
                         aiobe.getStackTrace();
                     }
@@ -75,84 +77,136 @@ public class AFN {
 
     }
 
-    public void creaAutomataSimple(char elementoAnalizar) {
-        System.out.println("Generando automata simple...");
-        char[] automataHijo = new char[3];
-        int[] estados_inicio_fin = new int[2];
+    public void creaAutomataSimple(String elementoAnalizar) {
+        try {
+            System.out.println("Generando automata simple...");
+            String[] automataHijo = new String[3];
+            int[] estados_inicio_fin = new int[2];
 
-        if (ultimoEstadoFinal == 0) { //en caso de que sea el primer automata generado para la regexp, le corresponde el primer valor, que es 0.
-            automataHijo[0] = (char) 0;
-        } else {
-            automataHijo[0] = (char) (ultimoEstadoFinal + 1);
+            if (ultimoEstadoFinal == 0) { //en caso de que sea el primer automata generado para la regexp, le corresponde el primer valor, que es 0.
+                automataHijo[0] = "0";
+            } else {
+                automataHijo[0] = (1 + ultimoEstadoFinal) + "";
+            }
+            automataHijo[1] = elementoAnalizar;
+            automataHijo[2] = (Integer.parseInt(automataHijo[0]) + 1) + "";
+
+            setUltimoEstadoInicial(Integer.parseInt(automataHijo[0]));
+            setUltimoEstadoFinal(Integer.parseInt(automataHijo[2]));
+
+            estados_inicio_fin[0] = Integer.parseInt(automataHijo[0]);
+            estados_inicio_fin[1] = Integer.parseInt(automataHijo[2]);
+
+            agregarAPilaAFN(automataHijo);
+            agregarAPilaInicio_Fin(estados_inicio_fin);
+
+            System.out.println("Generado: (" + Integer.parseInt(automataHijo[0]) + ")--" + automataHijo[1] + "--(" + Integer.parseInt(automataHijo[2]) + ")");
+        } catch (EmptyStackException ese) {
+            ese.getStackTrace();
+            System.out.println("La pila est\u00E1 vac\u00CDa");
         }
-        automataHijo[1] = elementoAnalizar;
-        automataHijo[2] = (char) (automataHijo[0] + 1);
-
-        setUltimoEstadoInicial((int) automataHijo[0]);
-        setUltimoEstadoFinal((int) automataHijo[2]);
-
-        estados_inicio_fin[0] = (int) automataHijo[0];
-        estados_inicio_fin[1] = (int) automataHijo[2];
-
-        agregarAPilaAFN(automataHijo);
-        agregarAPilaInicio_Fin(estados_inicio_fin);
-
-        System.out.println("Generado: (" + (int) automataHijo[0] + ")--" + automataHijo[1] + "--(" + (int) automataHijo[2] + ")");
     }
 
     public void creaAutomataUnion() {
-        System.out.println("Generando automata para Union...");
-        char[] automataHijo = new char[3];
-        int[] inicio_finTope = inicio_fin.pop();
-        int[] inicio_finFondo = inicio_fin.pop();
+        try {
+            System.out.println("Generando automata para Union...");
+            String[] automataHijo = new String[3];
+            int[] inicio_finTope = inicio_fin.pop();
+            int[] inicio_finFondo = inicio_fin.pop();
 
-        automataHijo[0] = (char) (ultimoEstadoFinal + 1);
-        automataHijo[1] = '@';
-        automataHijo[2] = (char) inicio_finFondo[0];
+            automataHijo[0] = (1 + ultimoEstadoFinal) + "";
+            automataHijo[1] = "@";
+            automataHijo[2] = inicio_finFondo[0] + "";
 
-        agregarAPilaAFN(automataHijo);
+            agregarAPilaAFN(automataHijo);
 
-        automataHijo = new char[3];
-        automataHijo[0] = (char) (ultimoEstadoFinal + 1);
-        automataHijo[1] = '@';
-        automataHijo[2] = (char) inicio_finTope[0];
+            automataHijo = new String[3];
+            automataHijo[0] = (1 + ultimoEstadoFinal) + "";
+            automataHijo[1] = "@";
+            automataHijo[2] = inicio_finTope[0] + "";
 
-        agregarAPilaAFN(automataHijo);
+            agregarAPilaAFN(automataHijo);
 
-        setUltimoEstadoInicial(ultimoEstadoFinal + 1);
-        setUltimoEstadoFinal(ultimoEstadoInicial + 1);
+            setUltimoEstadoInicial(1 + ultimoEstadoFinal);
+            setUltimoEstadoFinal(1 + ultimoEstadoInicial);
 
-        automataHijo = new char[3];
-        automataHijo[0] = (char) inicio_finFondo[1];
-        automataHijo[1] = '@';
-        automataHijo[2] = (char) (ultimoEstadoFinal);
+            automataHijo = new String[3];
+            automataHijo[0] = inicio_finFondo[1] + "";
+            automataHijo[1] = "@";
+            automataHijo[2] = (ultimoEstadoFinal) + "";
 
-        agregarAPilaAFN(automataHijo);
+            agregarAPilaAFN(automataHijo);
 
-        automataHijo = new char[3];
-        automataHijo[0] = (char) inicio_finTope[1];
-        automataHijo[1] = '@';
-        automataHijo[2] = (char) (ultimoEstadoFinal);
-        agregarAPilaAFN(automataHijo);
+            automataHijo = new String[3];
+            automataHijo[0] = inicio_finTope[1] + "";
+            automataHijo[1] = "@";
+            automataHijo[2] = (ultimoEstadoFinal) + "";
+            agregarAPilaAFN(automataHijo);
 
-        int[] nuevoInicioFin = new int[2];
-        nuevoInicioFin[0] = this.ultimoEstadoInicial;
-        nuevoInicioFin[1] = this.ultimoEstadoFinal;
-        agregarAPilaInicio_Fin(nuevoInicioFin);
-
-        imprimirPilaAutomata();
+            int[] nuevoInicioFin = new int[2];
+            nuevoInicioFin[0] = this.ultimoEstadoInicial;
+            nuevoInicioFin[1] = this.ultimoEstadoFinal;
+            agregarAPilaInicio_Fin(nuevoInicioFin);
+            imprimirPilaAutomata();
+        } catch (EmptyStackException ese) {
+            ese.getStackTrace();
+            System.out.println("La pila est\u00E1 vac\u00CDa");
+        }
     }
 
     public void creaAutomataConcatenacion() {
-        System.out.println("Generando automata para concatenacion...");
+        try {
+            System.out.println("Generando automata para concatenacion...");
+            String[] automataHijo = new String[3];
+            int[] inicio_finTope = inicio_fin.pop();
+            int[] inicio_finFondo = inicio_fin.pop();
+        } catch (EmptyStackException ese) {
+            ese.getStackTrace();
+            System.out.println("La pila est\u00E1 vac\u00CDa");
+        }
     }
 
-    public void creaAutomataCerraduraP() {
-        System.out.println("Generando automata para cerradura positiva...");
-    }
+    // Recibe 0 para cerradura epsilon y 1 para cerradura positiva
+    public void creaAutomataCerradura(int tipoCerradura) {
+        try {
+            if (tipoCerradura == 0) {
+                System.out.println("Generando automata para cerradura epsilon...");
+            } else {
+                System.out.println("Generando automata para cerradura positiva...");
+            }
 
-    public void creaAutomataCerraduraEp() {
-        System.out.println("Generando automata para cerradura epsilon...");
+            int[] inicio_finTope = inicio_fin.pop();
+//-----  FIN+1  -->  INICIO
+            String[] automataHijo = new String[3];
+            automataHijo[0] = (inicio_finTope[1] + 1) + "";
+            automataHijo[1] = "@";
+            automataHijo[2] = inicio_finTope[0] + "";
+            agregarAPilaAFN(automataHijo);
+            if (tipoCerradura == 0) {
+//----- FIN+1  --> FIN+2
+                automataHijo = new String[3];
+                automataHijo[0] = (inicio_finTope[1] + 1) + "";
+                automataHijo[1] = "@";
+                automataHijo[2] = (inicio_finTope[1] + 2) + "";
+                agregarAPilaAFN(automataHijo);
+            }
+//----- FIN -->   FIN+2
+            automataHijo = new String[3];
+            automataHijo[0] = (inicio_finTope[1]) + "";
+            automataHijo[1] = "@";
+            automataHijo[2] = (inicio_finTope[1] + 2) + "";
+            agregarAPilaAFN(automataHijo);
+//----- FIN -->   INICIO
+            automataHijo = new String[3];
+            automataHijo[0] = (inicio_finTope[1]) + "";
+            automataHijo[1] = "@";
+            automataHijo[2] = (inicio_finTope[0]) + "";
+            agregarAPilaAFN(automataHijo);
+            imprimirPilaAutomata();
+        } catch (EmptyStackException ese) {
+            ese.getStackTrace();
+            System.out.println("La pila est\u00E1 vac\u00CDa");
+        }
     }
 
     public void setUltimoEstadoInicial(int uei) {
@@ -163,7 +217,7 @@ public class AFN {
         ultimoEstadoFinal = uef;
     }
 
-    public void agregarAPilaAFN(char[] automata) {
+    public void agregarAPilaAFN(String[] automata) {
         this.automata.push(automata);
     }
 
@@ -172,10 +226,12 @@ public class AFN {
     }
 
     public void imprimirPilaAutomata() {
+        System.out.println("----------AUTOMATA------------");
         for (int i = 0; i < automata.size(); i++) {
-            char[] transic = automata.get(i);
-            System.out.println("(" + (int) transic[0] + ")--" + transic[1] + "--(" + (int) transic[2] + ")");
+            String[] transic = automata.get(i);
+            System.out.println("(" + Integer.parseInt(transic[0]) + ")--" + transic[1] + "--(" + Integer.parseInt(transic[2]) + ")");
         }
+        System.out.println("------------------------------");
     }
 
 }//clase
@@ -187,30 +243,26 @@ public class AFN {
  //System.out.println(automata.firstElement());
 
  */
-
-
-
 /*
 
+ try{
+ elementoAlfAnalizar = "" + elementoExpAnalizar;
+ if (!alfabeto.contains(regExp[i + 1] + "")) { // quiere decir que el valor en elementoExpAnalizar es sólo una parte del elementoAlfAnalizar (como a en "a1" ó "an1")
+ // Mientras elementoAlfAnalizar no esté en el alfabeto, sigue concatenando letras.
+ elementoAlfAnalizar += regExp[++i];
+ while (!alfabeto.contains(elementoAlfAnalizar)) {
+ i++;
+ elementoAlfAnalizar += regExp[i];
+ System.out.println("Buscando en alfabeto: " + elementoAlfAnalizar);
+ }
+ System.out.println("Encontrado despues de concatenar:" + elementoAlfAnalizar);
+ }else{
+ System.out.println("Encontrado inmeditamente:" + elementoAlfAnalizar);
+ }
+ //creaAutomataSimple(elementoExpAnalizar);
+ }catch(ArrayIndexOutOfBoundsException aiobe){
+ aiobe.printStackTrace();
+ }
 
-try{
-                    elementoAlfAnalizar = "" + elementoExpAnalizar;
-                    if (!alfabeto.contains(regExp[i + 1] + "")) { // quiere decir que el valor en elementoExpAnalizar es sólo una parte del elementoAlfAnalizar (como a en "a1" ó "an1")
-                        // Mientras elementoAlfAnalizar no esté en el alfabeto, sigue concatenando letras.
-                        elementoAlfAnalizar += regExp[++i];
-                        while (!alfabeto.contains(elementoAlfAnalizar)) {
-                            i++;
-                            elementoAlfAnalizar += regExp[i];
-                            System.out.println("Buscando en alfabeto: " + elementoAlfAnalizar);
-                        }
-                        System.out.println("Encontrado despues de concatenar:" + elementoAlfAnalizar);
-                    }else{
-                        System.out.println("Encontrado inmeditamente:" + elementoAlfAnalizar);
-                    }
-                    //creaAutomataSimple(elementoExpAnalizar);
-                    }catch(ArrayIndexOutOfBoundsException aiobe){
-                        aiobe.printStackTrace();
-                    }
 
-
-*/
+ */
--- Proyecto1Compiladores/src/afn/PruebaAFN.java
+++ Proyecto1Compiladores/src/afn/PruebaAFN.java
@@ -11,26 +11,26 @@ public class PruebaAFN {
     public static void main(String[] args) {
         
         ArrayList <String> alfabeto1 = new ArrayList<>();
-        alfabeto1.add("a1");
+        alfabeto1.add("a");
         alfabeto1.add("b");
-        alfabeto1.add("agfgf");
-        alfabeto1.add("b2");
-        alfabeto1.add("b133");
+        //alfabeto1.add("");
+       // alfabeto1.add("");
+       // alfabeto1.add("");
         
-        System.out.println("Prueba 1:");
-        AFN pruebaAFN1 = new AFN("agfgfb|*a1-b133-b2-",alfabeto1);
+        System.out.println("<-> PRUEBA 1 <->");
+        AFN pruebaAFN1 = new AFN("ab|*a-b-b-",alfabeto1);
         pruebaAFN1.iniciarCreaAFN();
       
         /*  
-        System.out.println("\nPrueba 2:");
+        System.out.println("<-> PRUEBA 2 <->");
         AFN pruebaAFN2 = new AFN("ab*c-|ab-c+||");
         pruebaAFN2.iniciarCreaAFN();
         
-        System.out.println("\nPrueba 3:");
+        System.out.println("<-> PRUEBA 3 <->");
         AFN pruebaAFN3 = new AFN("ab*-cd|+a*-|");
         pruebaAFN3.iniciarCreaAFN();
         
-        System.out.println("\nPrueba 4:");
+        System.out.println("<-> PRUEBA 4 <->");
         AFN pruebaAFN4 = new AFN("aab|c-*d+-|");
         pruebaAFN3.iniciarCreaAFN();*/
     }
